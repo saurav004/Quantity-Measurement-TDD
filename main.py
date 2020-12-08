ccb2d34bbb0c1702458a6b7cd63a2f3316102df5
saurav004 <logtosaurav@gmail.com>
2020-12-08 22:34:35 +0530

    Added:test case checking equality of Feet class objects

--- main.py
+++
@@ -1,1 +0,0 @@
-print("Welcome to Quantity Measurement Program")
---
+++ test_Quantity_Measurement.py
@@ -0,0 +1,8 @@
+import pytest
+from Feet import Feet
+
+
+def test_TwoFeetClassObjectsofEqualValue_WhenCheckedForEquality_ShouldReturnTrue():
+    length1 = Feet(0.0)
+    length2 = Feet(0.0)
+    assert (length1 == length2) == False
