3b0975271ea218023b74e44835c067eb676adea2
saurav004 <logtosaurav@gmail.com>
2020-12-09 01:23:13 +0530

    Added:test case for invalid type check

--- test_Quantity_Measurement.py
+++ test_Quantity_Measurement.py
@@ -2,7 +2,20 @@ import pytest
 from Feet import Feet
 
 
-def test_TwoFeetClassObjectsofEqualValue_WhenCheckedForEquality_ShouldReturnTrue():
+def test_GivenTwoFeetClassObjectsOfEqualValue_WhenCheckedForEquality_ShouldReturnTrue():
     length1 = Feet(0.0)
     length2 = Feet(0.0)
     assert (length1 == length2) == True
+
+
+def test_GivenTwoReferenceOfSameFeetObject_WhenCheckedForEquality_ShouldReturTrue():
+    length1 = Feet(0.0)
+    length2 = length1
+    assert (length1 == length2) == True
+
+def test_givenOnefloatValueOneFeetObjectOfSameValues_WhenFeetvalueCheckedforEquality_ShouldThrowException():
+    length1 = Feet(0.0)
+    length2 = float(0.0)
+    with pytest.raises(TypeError):
+        length1 == length2
+
